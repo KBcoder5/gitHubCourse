258419f7b6070af749b3f5814c4eb9c9953a2a99
Kbcoder5 <kush.beriwal@gmail.com>
2022-01-15 23:12:21 +0530

    added/modified/deleted files

--- src/Pages/HomePage.java
+++ src/Pages/HomePage.java
@@ -3,6 +3,6 @@ package Pages;
 public class HomePage {
 
 	public static void main(String args[]){
-		System.out.println("Home Page");		
+		System.out.println("Home Page Repo");		
 	}
 }
---
+++ src/Pages/searchPage.java
@@ -0,0 +1,12 @@
+package Pages;
+
+public class searchPage {
+	
+	public void search(){
+		System.out.println("Hello search Page");
+		String admin = "admin";
+		String password = "pass";
+		System.out.println("Login succesful");
+	}
+
+}
